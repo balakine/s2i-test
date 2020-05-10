5537de0c783ebe12ba4248419c93eaffaa171b0e
Victor Balakine <vbalakine@bestbuycanada.ca>
2020-05-09 22:07:27 -0700

    Added expressjs

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-var i = 1;
-setInterval(function(){ console.log("Log entry: " + i++); }, 5000);
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
